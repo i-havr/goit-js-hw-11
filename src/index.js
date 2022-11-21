363170ef8fdead6701d3622e5dc69e03da01f15a
i-havr <i.havrylov@gmail.com>
2022-11-21 21:59:00 +0200

    edit start markup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+console.log(refs.gallery);
